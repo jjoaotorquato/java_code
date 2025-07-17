113de61ccebf007be6043d6a79d7ee86cb641c60
jjoaotorquato <torquatojoao57@gmail.com>
2025-07-17 17:12:25 -0300

    java code

---
+++ src/count_characters.java
@@ -0,0 +1,9 @@
+public class count_characters {
+    public static void main(String[] args) {
+        String text = ("Hello World!!! This code show how many characters have in one text!!!");
+        int count_char = text.length();
+
+        System.out.println("TEXT HAVE "+count_char+ " CHARACTERS");
+    }
+    
+}
